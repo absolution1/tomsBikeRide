f8da05d75f5e209b6bc6a9b9865758b8195662ab
Matthew David Lawe <lawe@dyn-190-97.lancs.ac.uk>
2016-10-20 12:28:04 +0100

    Fun new plot

--- tomsBikeRide.C
+++ tomsBikeRide.C
@@ -1,4 +1,6 @@
 {
+  const double price = 900.00; // Cost of bike in pounds.
+  
   TString dir = gSystem->UnixPathName(__FILE__);
   dir.ReplaceAll("tomsBikeRide.C","");
   dir.ReplaceAll("/./","/");
@@ -6,7 +8,7 @@
   infile.open(Form("%stoms_bike_ride.txt",dir.Data()));
 
   int date;
-  double miles, speed, elevation;
+  double miles, speed, elevation, poundspermile;
 
   TFile *output = new TFile("output.root","RECREATE");
   TTree *tree = new TTree("tree","tree");
@@ -15,10 +17,37 @@
   tree->Branch("miles",&miles);
   tree->Branch("speed",&speed);
   tree->Branch("elevation",&elevation);
+  tree->Branch("poundspermile",&poundspermile);
 
+  double totalmiles = 0; // total miles accumulator
+  
   while (infile >> date >> miles >> speed >> elevation){
+    totalmiles+=miles;
+    poundspermile=price/totalmiles;
     tree->Fill();
   }
   tree->Write();
+
+  int n = tree->Draw("poundspermile:date","","goff");
+  TGraph *pricepermile = new TGraph(n, tree->GetV2(), tree->GetV1());
+  pricepermile->SetMarkerStyle(20);
+  pricepermile->GetXaxis()->SetTimeDisplay(1);
+  pricepermile->GetXaxis()->SetTitle("Date");
+  pricepermile->GetYaxis()->SetTitle("Cost (in GBP) for total accrued mileage");
+  pricepermile->SetTitle("");
+  pricepermile->Draw();
+
+  
+  std::cout<<totalmiles<<std::endl;
+
+  TText *t = new TText(.5,.5,Form("Total milage accrued: %.1f", totalmiles));
+  t->SetNDC();
+  t->SetTextAlign(22);
+  t->SetTextColor(kRed+2);
+  t->SetTextFont(43);
+  t->SetTextSize(40);
+  t->SetTextAngle(45);
+  t->Draw();
+
   output->Close();
 }
